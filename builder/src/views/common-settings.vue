11859f814b04c6aafe3aa06815cef99963d69c8d
Dmitry Konovalov <dkonovalov@selzy.com>
2022-11-22 16:04:52 +0300

    init

---
+++ builder/src/views/common-settings.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="b-unisender-app-common-settings b-unisender-app__common-settings">
+        <div class="b-unisender-app-common-settings__header">
+            <div class="b-unisender-app-common-settings__tab-buttons">
+                <div class="b-unisender-app-common-settings__tab-button"
+                     v-for="(item, index) in tabs.buttons"
+                     :key="`tab-button-${index}`"
+                     :class="index === activeTab ? 'active' : ''"
+                     @click="handleTabButtonClick(index)"
+                >
+                    {{item}}
+                </div>
+            </div>
+            <div class="b-unisender-app-common-settings__toggle-button"
+                 :class="showContent ? 'active' : ''"
+                 @click="tabContentShowButtonHandle">
+                <span></span>
+            </div>
+        </div>
+        <div class="b-unisender-app-common-settings__main"
+            :class="showContent ? 'active' : ''"
+        >
+            <div class="b-unisender-app-common-settings__tab-content"
+                :class="activeTab === 0 ? 'active' : ''"
+            >
+                <CommonSettingForm></CommonSettingForm>
+            </div>
+            <div class="b-unisender-app-common-settings__tab-content"
+                :class="activeTab === 1 ? 'active' : ''"
+            >
+                <CommonSettingMessages></CommonSettingMessages>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CommonSettingForm from '@/components/common-settings-form'
+import CommonSettingMessages from '@/components/common-settings-messages'
+export default {
+    components: {
+        CommonSettingForm,
+        CommonSettingMessages
+    },
+    data() {
+        return {
+            activeTab: 0,
+            tabs: {
+                buttons: ['Форма', 'Сообщения']
+            },
+            showContent: true
+        }
+    },
+    methods: {
+        handleTabButtonClick(index) {
+            this.activeTab = index
+        },
+        tabContentShowButtonHandle() {
+            this.showContent = !this.showContent
+        }
+    }
+}
+</script>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Unisender ===
+Contributors: unisender-inc
+Tags: unisender, subscribe
+Donate link: https://unisender.com/
+Requires at least: 5.0
+Tested up to: 5.0
+Requires PHP: 7.0
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+The plugin allows you to create a subscription form on your website and send user data to Unisender.
+
+== Description ==
+The plugin allows you to create a subscription form on your website and send user data to Unisender.
+
+All data of your subscribers will be sent to your account in Unisender.
+
+== Installation ==
+1. Install the plugin in wordpress.
+2. Generate Unisender API key: https://www.unisender.com/ru/support/api/common/api-key/.
+3. Create new subscription form.
+4. Configure the subscription field.
+5. Connect it to unisender.
+
+== Frequently Asked Questions ==
+For all questions mail plugins@unisender.com.
+
+== Screenshots ==
+1. Settings
+2. Form list
+3. Form settings
+
+== Changelog ==
+There are no changes in the operation of the application. The plugin works stably.
+
+== Upgrade Notice ==
+The latest version of the plugin works on the latest versions of wordpress.
